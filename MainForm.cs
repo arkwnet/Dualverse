04ff072c303d206bdba702f10fef0d9ede967d82
Sora Arakawa <mail@arkw.net>
2024-02-12 12:05:17 +0900

    Open link in default browser

--- MainForm.cs
+++ MainForm.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace Dualverse
 {
@@ -15,8 +17,10 @@ namespace Dualverse
 		{
 			await webView2Left.EnsureCoreWebView2Async(null);
 			webView2Left.CoreWebView2.Navigate("https://twitter.com/");
+			webView2Left.CoreWebView2.NewWindowRequested += NewWindowRequested;
 			await webView2Right.EnsureCoreWebView2Async(null);
 			webView2Right.CoreWebView2.Navigate("https://misskey.io/");
+			webView2Right.CoreWebView2.NewWindowRequested += NewWindowRequested;
 		}
 
 		private void MainForm_Resize(object sender, System.EventArgs e)
@@ -27,6 +31,17 @@ namespace Dualverse
 		void ResizeComponent()
 		{
 			webView2Left.Width = this.Width / 2;
+		}
+
+		private void NewWindowRequested(object sender, CoreWebView2NewWindowRequestedEventArgs e)
+		{
+			e.Handled = true;
+			ProcessStartInfo processStartInfo = new ProcessStartInfo()
+			{
+				FileName = e.Uri,
+				UseShellExecute = true,
+			};
+			Process.Start(processStartInfo);
 		}
 	}
 }
