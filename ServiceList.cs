1a4f285ff4c8226dca6e13517f3e21e92f97b818
Sora Arakawa <mail@arkw.net>
2024-06-26 23:06:14 +0900

    Expand service list

--- ServiceList.cs
+++ ServiceList.cs
@@ -9,7 +9,12 @@ namespace Dualverse
 		public ServiceList() {
 			serviceList.Add(new Service("X (旧Twitter)", "https://twitter.com/"));
 			serviceList.Add(new Service("X Pro (旧TweetDeck)", "https://pro.twitter.com/"));
+			serviceList.Add(new Service("Bluesky", "https://bsky.app/"));
+			serviceList.Add(new Service("Threads", "https://threads.net/"));
 			serviceList.Add(new Service("Misskey.io", "https://misskey.io/"));
+			serviceList.Add(new Service("mstdn.jp", "https://mstdn.jp/"));
+			serviceList.Add(new Service("Pawoo", "https://pawoo.net/"));
+			serviceList.Add(new Service("fedibird", "https://fedibird.com/"));
 			serviceList.Add(new Service("カスタム", ""));
 		}
 
